f9ad5c02384bdf556e5be3df56e7d105efab551c
Robert Reinhardt <34960920+robs2243@users.noreply.github.com>
2025-11-19 10:15:58 +0100

    first commit

---
+++ PongGame.cs
@@ -0,0 +1,25 @@
+﻿using Raylib_cs;
+
+namespace pong;
+
+internal static class PongGame
+{
+    // STAThread is required if you deploy using NativeAOT on Windows - See https://github.com/raylib-cs/raylib-cs/issues/301
+    [System.STAThread]
+    public static void Main()
+    {
+        Raylib.InitWindow(800, 480, "Hello World");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.DrawText("Hello, world!", 12, 12, 20, Color.Black);
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+}
